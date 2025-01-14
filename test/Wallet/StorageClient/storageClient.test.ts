eaf92a06aa40d3ee5d5ab344b2a0258a2470f62a
Tone Engel <tone@kizmet.org>
2025-01-14 15:55:50 -0500

    wspublish

--- test/Wallet/StorageClient/storageClient.test.ts
+++ test/Wallet/StorageClient/storageClient.test.ts
@@ -27,6 +27,9 @@ describe('walletStorageClient test', () => {
     test('1_backup to client', async () => {
         for (const { wallet, storage } of ctxs) {
             {
+                const privKey = bsv.PrivateKey.fromString('e98e70d5610369a675645db2ccc1570443a7058950816e0c7c78227b0451f70f', 'hex')
+                const identityKey = privKey.toPublicKey().toString()
+                
                 const client = new StorageClient(wallet as unknown as bsv.Wallet, 'https://staging-dojo.babbage.systems')
                 const s = await client.makeAvailable()
                 storage.stores.push(client)

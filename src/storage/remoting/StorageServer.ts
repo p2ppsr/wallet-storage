70f9d59e10c82f351eecde791e772fc4b121f5a9
Brayden Langley <brayden167@gmail.com>
2025-01-14 11:32:01 -0800

    Conditionally apply auth/payment middleware

--- src/storage/remoting/StorageServer.ts
+++ src/storage/remoting/StorageServer.ts
@@ -41,84 +41,94 @@ export class StorageServer {
 
   private setupRoutes(): void {
     this.app.use(express.json())
-    const options: AuthMiddlewareOptions = {
-      wallet: this.wallet
-    }
-    this.app.use(createAuthMiddleware(options))
-    if (this.monetize) {
-      this.app.use(
-        createPaymentMiddleware({
-          wallet: this.wallet,
-          calculateRequestPrice: () => 100
-        })
-      )
-    }
 
     // A single POST endpoint for JSON-RPC:
-    this.app.post('/', async (req: Request, res: Response) => {
-      debugger
-      let { jsonrpc, method, params, id } = req.body
-      if (method !== 'getSettings') {
-        if (typeof params[0] !== 'object' || !params[0]) {
-          params = [{}]
+    this.app.post('/',
+      // Conditionally run auth middleware
+      (req: Request, res: Response, next: Function) => {
+        const { method } = req.body
+        if (method === 'getSettings') {
+          // Skip auth
+          return next()
         }
-      }
+        return createAuthMiddleware({ wallet: this.wallet })(req, res, next)
+      },
 
-      // Basic JSON-RPC protocol checks:
-      if (jsonrpc !== '2.0' || !method || typeof method !== 'string') {
-        return res.status(400).json({ error: { code: -32600, message: 'Invalid Request' } })
-      }
-
-      try {
-        // Dispatch the method call:
-        if (typeof (this as any)[method] === 'function') {
-          // if you wanted to handle certain methods on the server class itself
-          // e.g. this['someServerMethod'](params)
-          throw new Error('Server method dispatch not used in this approach.')
-        } else if (typeof (this.walletStorage as any)[method] === 'function') {
-          // method is on the walletStorage:
-          // Find user
-          if (method !== 'getSettings') {
-            const user = await this.walletStorage.findUserByIdentityKey(req.auth.identityKey)
-            if (!user) {
-              const userId = await this.walletStorage.insertUser({
-                identityKey: req.auth.identityKey as string,
-                userId: 0,
-                created_at: new Date(),
-                updated_at: new Date()
-              })
-              params[0].userId = userId
-            } else {
-              params[0].userId = user.userId
-            }
+      // Conditionally run payment middleware
+      (req: Request, res: Response, next: Function) => {
+        const { method } = req.body
+        if (!this.monetize || method === 'getSettings') {
+          // Skip payment middleware for "getSettings" or if monetization is disabled
+          return next()
+        }
+        return createPaymentMiddleware({
+          wallet: this.wallet,
+          calculateRequestPrice: () => 100
+        })(req, res, next)
+      },
+      async (req: Request, res: Response) => {
+        let { jsonrpc, method, params, id } = req.body
+        if (method !== 'getSettings') {
+          if (typeof params[0] !== 'object' || !params[0]) {
+            params = [{}]
           }
-          const result = await (this.walletStorage as any)[method](...(params || []))
-          return res.json({ jsonrpc: '2.0', result, id })
-        } else {
-          // Unknown method
-          return res.status(400).json({
+        }
+
+        // Basic JSON-RPC protocol checks:
+        if (jsonrpc !== '2.0' || !method || typeof method !== 'string') {
+          return res.status(400).json({ error: { code: -32600, message: 'Invalid Request' } })
+        }
+
+        try {
+          // Dispatch the method call:
+          if (typeof (this as any)[method] === 'function') {
+            // if you wanted to handle certain methods on the server class itself
+            // e.g. this['someServerMethod'](params)
+            throw new Error('Server method dispatch not used in this approach.')
+          } else if (typeof (this.walletStorage as any)[method] === 'function') {
+            // method is on the walletStorage:
+            // Find user
+            if (method !== 'getSettings') {
+              const user = await this.walletStorage.findUserByIdentityKey(req.auth.identityKey)
+              if (!user) {
+                const userId = await this.walletStorage.insertUser({
+                  identityKey: req.auth.identityKey as string,
+                  userId: 0,
+                  created_at: new Date(),
+                  updated_at: new Date()
+                })
+                params[0].userId = userId
+              } else {
+                params[0].userId = user.userId
+              }
+            }
+            const result = await (this.walletStorage as any)[method](...(params || []))
+            return res.json({ jsonrpc: '2.0', result, id })
+          } else {
+            // Unknown method
+            return res.status(400).json({
+              jsonrpc: '2.0',
+              error: { code: -32601, message: `Method not found: ${method}` },
+              id
+            })
+          }
+        } catch (error) {
+          // Catch any thrown errors from the local walletStorage method
+          const err = error as Error
+          return res.status(200).json({
             jsonrpc: '2.0',
-            error: { code: -32601, message: `Method not found: ${method}` },
+            error: {
+              code: -32000,
+              message: err.message,
+              data: {
+                name: err.name,
+                stack: err.stack
+              }
+            },
             id
           })
         }
-      } catch (error) {
-        // Catch any thrown errors from the local walletStorage method
-        const err = error as Error
-        return res.status(200).json({
-          jsonrpc: '2.0',
-          error: {
-            code: -32000,
-            message: err.message,
-            data: {
-              name: err.name,
-              stack: err.stack
-            }
-          },
-          id
-        })
-      }
-    })
+      })
   }
 
   public start(): void {

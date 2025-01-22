b186878b9d5a8b7b8b2540355ad0443d30e65949
Tone Engel <tone@kizmet.org>
2025-01-22 08:44:47 -0500

    add second pass to confirm quick incremental syncing

--- test/wallet/sync/Wallet.sync.test.ts
+++ test/wallet/sync/Wallet.sync.test.ts
@@ -9,85 +9,77 @@ describe('Wallet sync tests', () => {
     jest.setTimeout(99999999)
 
     const env = _tu.getEnv('test')
-    const ctxs: TestWalletNoSetup[] = []
 
     beforeAll(async () => {
-        if (!env.noMySQL)
-            ctxs.push(await _tu.createLegacyWalletMySQLCopy('walletSyncTestSource'));
-        ctxs.push(await _tu.createLegacyWalletSQLiteCopy('walletSyncTestSource'))
     })
 
     afterAll(async () => {
-        for (const ctx of ctxs) {
-            await ctx.storage.destroy()
-        }
     })
 
 
     test('0 syncToWriter initial-no changes-1 change', async () => {
-        for (const { identityKey, activeStorage: storage } of ctxs) {
-            const localSQLiteFile = await _tu.newTmpFile('walleltSyncTest0tmp.sqlite', true, false, false)
-            const knexSQLite = _tu.createLocalSQLite(localSQLiteFile)
-            const tmpStore = new StorageKnex({...StorageKnex.defaultOptions(), chain: env.chain, knex: knexSQLite })
-            //await tmpStore.dropAllData()
-            await tmpStore.migrate('walletSyncTest0tmp', '1'.repeat(64))
-            const dstSettings = await tmpStore.makeAvailable()
+        const { identityKey, storage: storageManager, activeStorage: storage } = await _tu.createLegacyWalletSQLiteCopy('walletSyncTest1aSource')
+        const localSQLiteFile = await _tu.newTmpFile('walleltSyncTest0tmp.sqlite', true, false, false)
+        const knexSQLite = _tu.createLocalSQLite(localSQLiteFile)
+        const tmpStore = new StorageKnex({ ...StorageKnex.defaultOptions(), chain: env.chain, knex: knexSQLite })
+        //await tmpStore.dropAllData()
+        await tmpStore.migrate('walletSyncTest0tmp', '1'.repeat(64))
+        const dstSettings = await tmpStore.makeAvailable()
 
-            const srcSettings = await storage.makeAvailable()
-            const manager = new WalletStorageManager(identityKey, storage, [tmpStore])
+        const srcSettings = await storage.makeAvailable()
+        const manager = new WalletStorageManager(identityKey, storage, [tmpStore])
 
-            const auth = await manager.getAuth()
-            {
-                const r = await manager.syncToWriter(auth, tmpStore)
-                expect(r.inserts).toBeGreaterThan(1000)
-                expect(r.updates).toBe(0)
-            }
-            {
-                const r = await manager.syncToWriter(auth, tmpStore)
-                expect(r.inserts).toBe(0)
-                expect(r.updates).toBe(0)
-            }
-            {
-                _tu.insertTestOutputBasket(storage, auth.userId)
-                await wait(1000)
-                const r = await manager.syncToWriter(auth, tmpStore)
-                expect(r.inserts).toBe(1)
-                expect(r.updates).toBe(0)
-            }
-            await tmpStore.destroy()
+        const auth = await manager.getAuth()
+        {
+            const r = await manager.syncToWriter(auth, tmpStore)
+            expect(r.inserts).toBeGreaterThan(1000)
+            expect(r.updates).toBe(0)
         }
+        {
+            const r = await manager.syncToWriter(auth, tmpStore)
+            expect(r.inserts).toBe(0)
+            expect(r.updates).toBe(0)
+        }
+        {
+            _tu.insertTestOutputBasket(storage, auth.userId)
+            await wait(1000)
+            const r = await manager.syncToWriter(auth, tmpStore)
+            expect(r.inserts).toBe(1)
+            expect(r.updates).toBe(0)
+        }
+        await tmpStore.destroy()
+        await storageManager.destroy()
     })
 
     test('1a setActive to backup and back to original without backup first', async () => {
         // wallet will be the original active wallet, a backup is added, then setActive is used to initiate backup in each direction.
-        for (const ctx of ctxs) {
+        const ctx = await _tu.createLegacyWalletSQLiteCopy('walletSyncTest1aSource')
+        const backup = (await _tu.createSQLiteTestWallet({ databaseName: 'walletSyncTest1aBackup', dropAll: true })).activeStorage;
+        await ctx.storage.addWalletStorageProvider(backup);
 
-          await setActiveTwice(ctx, false);
+        await setActiveTwice(ctx, false, backup);
 
-        }
-      })
+        await setActiveTwice(ctx, false, backup);
+
+        await ctx.storage.destroy()
+    })
 
     test('1b setActive to backup and back to original with backup first', async () => {
         // wallet will be the original active wallet, a backup is added, then setActive is used to initiate backup in each direction.
-        for (const ctx of ctxs) {
+        const ctx = await _tu.createLegacyWalletSQLiteCopy('walletSyncTest1bSource')
+        const backup = (await _tu.createSQLiteTestWallet({ databaseName: 'walletSyncTest1bBackup', dropAll: true })).activeStorage;
+        await ctx.storage.addWalletStorageProvider(backup);
 
-          await setActiveTwice(ctx, true);
+        await setActiveTwice(ctx, true, backup);
 
-        }
-      })
+        await setActiveTwice(ctx, true, backup);
+
+        await ctx.storage.destroy()
+    })
 })
 
-async function setActiveTwice(ctx: TestWalletNoSetup, withBackupFirst: boolean) {
+async function setActiveTwice(ctx: TestWalletNoSetup, withBackupFirst: boolean, backup: StorageKnex) {
     const { storage: storageManager, activeStorage: original, userId: originalUserId } = ctx;
-
-    const backup = (await _tu.createSQLiteTestWallet({ databaseName: 'walletSyncTest1aBackup', dropAll: true })).activeStorage;
-
-    if (originalUserId === 1) {
-        // Inert a dummy user into backup to make sure the userId isn't same as original
-        await backup.findOrInsertUser('99'.repeat(32));
-    }
-
-    await storageManager.addWalletStorageProvider(backup);
 
     if (withBackupFirst) {
         await storageManager.updateBackups()
@@ -115,8 +107,8 @@ async function setActiveTwice(ctx: TestWalletNoSetup, withBackupFirst: boolean) 
     let originalUserAfter = verifyTruthy(await original.findUserById(originalAuth.userId!));
     let backupUserAfter = verifyOne(await backup.findUsers({ partial: { identityKey: originalAuth.identityKey } }));
 
-    expect(originalUserAfter.updated_at.getTime()).toBeGreaterThan(now);
-    expect(backupUserAfter.updated_at.getTime()).toBeGreaterThan(now);
+    expect(originalUserAfter.updated_at.getTime()).toBeGreaterThanOrEqual(now);
+    expect(backupUserAfter.updated_at.getTime()).toBeGreaterThanOrEqual(now);
     expect(originalUserAfter.activeStorage).toBe(backupIdentityKey);
     expect(backupUserAfter.activeStorage).toBe(backupIdentityKey);
 
